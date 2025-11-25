dfa7595c6d6aa18d878ea467e49a7778aa8f1429
edujunior2010outlookcom-max <edujunior2010outlook.com@gmail.com>
2025-11-25 20:47:23 -0300

    Implementei storage_usuario
    Implement functions to load and save user data in a JSON file.

--- storage.py
+++ storage.py
@@ -1,1 +1,29 @@
+import json
+import os
+
+
+CAMINHO_DATA = os.path.join(os.path.dirname(__file__), "data")
+
+
+os.makedirs(CAMINHO_DATA, exist_ok=True)
+
+CAMINHO_USUARIOS = os.path.join(CAMINHO_DATA, "usuarios.json")
+
+
+def carregar_usuarios():
+    """Lê o arquivo usuarios.json e retorna a lista de usuários."""
+    try:
+        with open(CAMINHO_USUARIOS, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return [] 
+
+
+def salvar_usuarios(lista_usuarios):
+    """Salva a lista de usuários no arquivo usuarios.json."""
+    with open(CAMINHO_USUARIOS, "w", encoding="utf-8") as f:
+        json.dump(lista_usuarios, f, indent=4, ensure_ascii=False)
+
 
