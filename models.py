e1a09e83e28bf2f8258e7529baf98d25d28d6d37
edujunior2010outlookcom-max <edujunior2010outlook.com@gmail.com>
2025-11-25 20:46:09 -0300

    Implementei models_usuario
    Add base structure for user model with basic attributes.

--- models.py
+++ models.py
@@ -1,1 +1,15 @@
+## -------------USUARIO-------------
+def modelo_usuario(nome, email, sexo, idade, cpf):
+    """
+    Estrutura base de um usuário.
+    Nenhuma validação ocorre aqui.
+    """
+    return {
+        "nome": nome,
+        "email": email.lower(),
+        "sexo": sexo.upper(),
+        "idade": int(idade),
+        "cpf": cpf
+    }
+
 
