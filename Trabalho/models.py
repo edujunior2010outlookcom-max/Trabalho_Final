07c041a4cea40da478ef2771c0f32eb3bdc76d9f
edujunior2010outlookcom-max <edujunior2010outlook.com@gmail.com>
2025-11-27 09:17:12 -0300

    Refactor modelo_tarefa and modelo_projeto functions

--- Trabalho/models.py
+++ Trabalho/models.py
@@ -12,21 +12,20 @@ def modelo_usuario(nome, email, sexo, idade, cpf):
     }
     
 
-# TAREFAS
+
 def modelo_tarefa(titulo, projeto, responsavel, status, prazo):
     """
     Estrutura base de uma tarefa.
     """
     return {
         "titulo": titulo,
-        "projeto": projeto,           # nome do projeto ao qual pertence
-        "responsavel": responsavel,   # e-mail ou nome do usuário
-        "status": status,             # pendente / andamento / concluída
-        "prazo": prazo                # YYYY-MM-DD (string)
+        "projeto": projeto,           
+        "responsavel": responsavel,   
+        "status": status,             
+        "prazo": prazo                
     }
 
 
-#  PROJETOS 
 def modelo_projeto(nome, descricao, inicio, fim):
     """
     Estrutura base de um projeto.
@@ -34,6 +33,6 @@ def modelo_projeto(nome, descricao, inicio, fim):
     return {
         "nome": nome,
         "descricao": descricao,
-        "inicio": inicio,   # datas no formato YYYY-MM-DD ou DD/MM/YYYY
+        "inicio": inicio,   
         "fim": fim
     }
